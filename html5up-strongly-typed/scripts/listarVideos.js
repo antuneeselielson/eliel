5fa4eea4937d1f536a5f775ef0f64feeeece787a
Abel Antunes <34608001+abelantunes98@users.noreply.github.com>
2019-09-17 01:55:20 -0300

    Pasta scripts

---
+++ html5up-strongly-typed/scripts/listarVideos.js
@@ -0,0 +1,10 @@
+
+assync function listarVideos () {
+    
+}
+
+
+<object width="425" height="350">
+<param name="movie" value=link />
+<embed src="http://www.youtube.com/v/tIBxavsiHzM" type="application/x-shockwave-flash" width="425" height="350" />
+</object>
